b704263afbf66565496c2c5f8f4940d83d2f85dd
Марсель Адилов <marseladilov@MacBook-Pro-Marsel.local>
2025-05-30 21:26:14 +0500

    Added Windows support

--- Get-Archive.Tests.ps1
+++ Get-Archive.Tests.ps1
@@ -1,19 +1,17 @@
 BeforeAll {
-
-		# Creating a test directory
-		$baseDir = "dev_build_test"
-		$components = @("grpedit", "modservice", "rsysconf", "scada")
+	# Creating a test directory
+	$baseDir = "dev_build_test"
+	$components = @("grpedit", "modservice", "rsysconf", "scada")
 		
-		foreach ($component in $components) {
-		    $compPath = Join-Path $baseDir $component
-		    $testsPath = Join-Path $compPath "tests"
-		    New-Item -ItemType Directory -Path $testsPath -Force | Out-Null
-		    New-Item -ItemType File -Path (Join-Path $compPath "distr.deb") -Force | Out-Null
-		    New-Item -ItemType File -Path (Join-Path $compPath "syms.7z") -Force | Out-Null
-		    New-Item -ItemType File -Path (Join-Path $testsPath "test_reports.txt.7z") -Force | Out-Null
-		    New-Item -ItemType File -Path (Join-Path $testsPath "test_reports.xunit.7z") -Force | Out-Null
-		}
-		
+	foreach ($component in $components) {
+		$compPath = Join-Path $baseDir $component
+		$testsPath = Join-Path $compPath "tests"
+		New-Item -ItemType Directory -Path $testsPath -Force | Out-Null
+		New-Item -ItemType File -Path (Join-Path $compPath "distr.deb") -Force | Out-Null
+		New-Item -ItemType File -Path (Join-Path $compPath "syms.7z") -Force | Out-Null
+		New-Item -ItemType File -Path (Join-Path $testsPath "test_reports.txt.7z") -Force | Out-Null
+		New-Item -ItemType File -Path (Join-Path $testsPath "test_reports.xunit.7z") -Force | Out-Null
+	}
 		
     . $PSScriptRoot/Get-Archive.ps1
 		$result = Get-Archive -root_dir "./dev_build_test"
@@ -21,36 +19,35 @@ BeforeAll {
 }
 
 Describe 'Get-Archive' {
- 
-		It 'Проекты найдены' {
-		    $projectsNumber | Should -BeGreaterThan 0
+	It 'Projects are found' {
+		$projectsNumber | Should -BeGreaterThan 0
     }
-    It 'Созданы архивы' {
-		    $result.CreatedArchives.Count | Should -Be $projectsNumber
-		    foreach ($i in 0..($projectsNumber-1)) {
-			    $result.CreatedArchives[$i] | Should -Be "$($result.Projects[$i])_artifacts.7z"
-		    }
+    It 'Archives have been created correctly' {
+		$result.CreatedArchives.Count | Should -Be $projectsNumber
+		foreach ($i in 0..($projectsNumber-1)) {
+			$result.CreatedArchives[$i] | Should -Be "$($result.Projects[$i])_artifacts.7z"
+		}
     }
-    It 'Хэш-суммы MD5 созданы корректно' {
-		    $result.CreatedArchives.Count | Should -Be $projectsNumber
-		    foreach ($i in 0..($projectsNumber-1)) {
-			    $result.CreatedMD5[$i] | Should -Be "$($result.CreatedArchives[$i]).md5"
-			    Get-Content $result.CreatedMD5[$i] | Should -Be (Get-FileHash $result.CreatedArchives[$i] MD5).Hash
-		    }
+    It 'MD5 hash sums have been created correctly' {
+		$result.CreatedArchives.Count | Should -Be $projectsNumber
+		foreach ($i in 0..($projectsNumber-1)) {
+			$result.CreatedMD5[$i] | Should -Be "$($result.CreatedArchives[$i]).md5"
+			Get-Content $result.CreatedMD5[$i] | Should -Be (Get-FileHash $result.CreatedArchives[$i] -Algorithm MD5).Hash
+		}
     }
-    It 'Хэш-суммы SHA1 созданы корректно' {
-		    $result.CreatedArchives.Count | Should -Be $projectsNumber
-		    foreach ($i in 0..($projectsNumber-1)) {
-			    $result.CreatedSHA1[$i] | Should -Be "$($result.CreatedArchives[$i]).sha1"
-			    Get-Content $result.CreatedSHA1[$i] | Should -Be (Get-FileHash $result.CreatedArchives[$i] SHA1).Hash
-		    }
+    It 'SHA1 hash sums have been created correctly' {
+		$result.CreatedArchives.Count | Should -Be $projectsNumber
+		foreach ($i in 0..($projectsNumber-1)) {
+			$result.CreatedSHA1[$i] | Should -Be "$($result.CreatedArchives[$i]).sha1"
+			Get-Content $result.CreatedSHA1[$i] | Should -Be (Get-FileHash $result.CreatedArchives[$i] -Algorithm SHA1).Hash
+		}
     }
-    It 'Хэш-суммы SHA256 созданы корректно' {
-		    $result.CreatedArchives.Count | Should -Be $projectsNumber
-		    foreach ($i in 0..($projectsNumber-1)) {
-			    $result.CreatedSHA256[$i] | Should -Be "$($result.CreatedArchives[$i]).sha256"
-			    Get-Content $result.CreatedSHA256[$i] | Should -Be (Get-FileHash $result.CreatedArchives[$i] SHA256).Hash
-		    }
+    It 'SHA256 hash sums have been created correctly' {
+		$result.CreatedArchives.Count | Should -Be $projectsNumber
+		foreach ($i in 0..($projectsNumber-1)) {
+			$result.CreatedSHA256[$i] | Should -Be "$($result.CreatedArchives[$i]).sha256"
+			Get-Content $result.CreatedSHA256[$i] | Should -Be (Get-FileHash $result.CreatedArchives[$i] -Algorithm SHA256).Hash
+		}
     }
 }
 
--- Get-Archive.ps1
+++ Get-Archive.ps1
@@ -3,7 +3,7 @@ param (
 )
 
 function Get-Archive ([string]$root_dir) {
-	$projects = Get-ChildItem -Attributes Directory $root_dir
+	$projects = Get-ChildItem -Attributes Directory -Path $root_dir | ForEach-Object { $_.FullName }
 	
 	$createdFiles = @()
 	# Temp folder for hash sums
@@ -11,29 +11,35 @@ function Get-Archive ([string]$root_dir) {
 	New-Item -Path $tmpHashDir -ItemType Directory -Force | Out-Null
 	
 	foreach ($project in $projects) {
-				  # Creating archive
-	        $archive_name = "${project}_artifacts.7z"
-	        # Adding hash sums for all project's files
-	        Get-ChildItem -Path $project -Recurse -File | Get-FileHash -Algorithm MD5 | Out-File "${tmpHashDir}/md5sums.txt"
-	        Get-ChildItem -Path $project -Recurse -File | Get-FileHash -Algorithm SHA1 | Out-File "${tmpHashDir}/sha1sums.txt"
-	        Get-ChildItem -Path $project -Recurse -File | Get-FileHash -Algorithm SHA256 | Out-File "${tmpHashDir}/sha256sums.txt"
-	        7z a $archive_name $project "${tmpHashDir}/*" -bso0
-	
-	        # Archive's hash sums
-	        $md5 = "${archive_name}.md5"
-	        $sha1 = "${archive_name}.sha1"
-	        $sha256 = "${archive_name}.sha256"
-	        (Get-FileHash $archive_name MD5).Hash | Out-File -Encoding ASCII $md5
-	        (Get-FileHash $archive_name SHA1).Hash | Out-File -Encoding ASCII $sha1
-	        (Get-FileHash $archive_name SHA256).Hash | Out-File -Encoding ASCII $sha256
-	        
-	        $createdArchives += @($archive_name)
-	        $createdMD5 += @($md5)
-	        $createdSHA1 += @($sha1)
-	        $createdSHA256 += @($sha256)
+		# Creating archive
+		$archive_name = "${project}_artifacts.7z"
+		# Adding hash sums for all project's files
+		Get-ChildItem -Path $project -Recurse -File | ForEach-Object {
+			$hash = Get-FileHash -Algorithm MD5 -Path $_.FullName 
+			"$($hash.Hash)  $($hash.Path)"} | Out-File -Encoding UTF8 "${tmpHashDir}/md5sums.txt"
+		Get-ChildItem -Path $project -Recurse -File | ForEach-Object {
+			$hash = Get-FileHash -Algorithm SHA1 -Path $_.FullName 
+			"$($hash.Hash)  $($hash.Path)"} | Out-File -Encoding UTF8 "${tmpHashDir}/sha1sums.txt"
+		Get-ChildItem -Path $project -Recurse -File | ForEach-Object {
+			$hash = Get-FileHash -Algorithm SHA256 -Path $_.FullName 
+			"$($hash.Hash)  $($hash.Path)"} | Out-File -Encoding UTF8 "${tmpHashDir}/sha256sums.txt"
+		7z a $archive_name $project "${tmpHashDir}/*" -bso0
+
+		# Archive's hash sums
+		$md5 = "${archive_name}.md5"
+		$sha1 = "${archive_name}.sha1"
+		$sha256 = "${archive_name}.sha256"
+		(Get-FileHash $archive_name -Algorithm MD5).Hash | Out-File -Encoding ASCII $md5
+		(Get-FileHash $archive_name -Algorithm SHA1).Hash | Out-File -Encoding ASCII $sha1
+		(Get-FileHash $archive_name -Algorithm SHA256).Hash | Out-File -Encoding ASCII $sha256
+		
+		$createdArchives += @($archive_name)
+		$createdMD5 += @($md5)
+		$createdSHA1 += @($sha1)
+		$createdSHA256 += @($sha256)
 	}
 	Remove-Item $tmpHashDir -Recurse -Force
-	
+
 	return @{
         Projects = $projects
         CreatedArchives = $createdArchives
